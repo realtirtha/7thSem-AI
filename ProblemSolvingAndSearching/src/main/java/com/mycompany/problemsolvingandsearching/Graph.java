c895521eec30c30c6a14365ebeab2a925eddecc9
Tirth <tirthacodes@gmail.com>
2022-12-02 08:44:57 +0545

    december 2 lab

--- ProblemSolvingAndSearching/src/main/java/com/mycompany/problemsolvingandsearching/Graph.java
+++ ProblemSolvingAndSearching/src/main/java/com/mycompany/problemsolvingandsearching/Graph.java
@@ -14,4 +14,19 @@ public class Graph {
         adjacencyMap = new HashMap<>();
         directed= dir;
     }
+    public void insertEdge(Node source, Node target){
+        if(!adjacencyMap.keySet().contains(source)){
+            LinkedList<Node> tmp= new LinkedList<>();
+            tmp.add(target);
+            adjacencyMap.put(source, tmp);
+        }
+        else{
+            LinkedList<Node> tmp = adjacencyMap.get(source);
+            tmp.add(target);
+            adjacencyMap.put(source, tmp);
+        }
+    }
+    public void printEdges(){
+        
+    }
 }
--- ProblemSolvingAndSearching/src/main/java/com/mycompany/problemsolvingandsearching/ProblemSolvingAndSearching.java
+++ ProblemSolvingAndSearching/src/main/java/com/mycompany/problemsolvingandsearching/ProblemSolvingAndSearching.java
@@ -9,6 +9,9 @@ public class ProblemSolvingAndSearching {
     public static void main(String[] args) {
         Node n1= new Node(1,"Arad",false);
         Node n2= new Node(2,"zerind",false);
+        Node n3= new Node(3, "ktm",false);
         Graph graph= new Graph(false);
+        graph.insertEdge(n1,n2);
+        graph.insertEdge(n1,n3);
     }
 }
